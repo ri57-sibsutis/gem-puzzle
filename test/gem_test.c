90b14243def6c58735ee56bbf24b1ca880a9a618
DashProk <dashkos-kokos@mail.ru>
2016-05-27 22:40:36 +0600

    First test is done

---
+++ test/gem_test.c
@@ -0,0 +1,21 @@
+#include <ctest.h>
+
+#include <func.h>
+
+CTEST(gem_suite, search_of_zero_test)
+{
+	//Given
+	const int n = 4;
+	const int field[4][4] = {{12, 5, 8, 7}, {11, 2, 4, 14}, {13, 6, 1, 0}, {10, 9, 15, 3}};
+
+	//When
+	int a = 0,  b = 0;
+	search_of_zero(n, field, &a, &b);
+
+	//Then
+	const int expected_a = 2;
+	const int expected_b = 3;
+
+	ASSERT_EQUAL(expected_a, a);
+	ASSERT_EQUAL(expected_b, b);
+}

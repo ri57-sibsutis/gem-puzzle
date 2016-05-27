f96bcb99a8f75ab9f4222555e5042e2d37d4c507
DashProk <dashkos-kokos@mail.ru>
2016-05-28 04:26:46 +0600

    Correct makefile9

--- test/gem_test.c
+++ test/gem_test.c
@@ -94,7 +94,7 @@ CTEST(gem_suite, amount_test)
 {
 	//Given
 	const int n = 4;
-	int field[4][4] = {{12, 5, 8, 7}, {4,11, 2, 14}, {13, 6, 1, 0}, {10, 9, 15, 3}};
+	int field[4][4] = {{12, 5, 8, 7}, {11, 2, 4, 14}, {13, 6, 1, 0}, {10, 9, 15, 3}};
 	const int i = 1;
 	const int j = 0;
 

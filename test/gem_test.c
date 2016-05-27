f0fa384570e615e35768ba5fbc599fae24e2833c
DashProk <dashkos-kokos@mail.ru>
2016-05-28 00:02:29 +0600

    trying3

--- test/gem_test.c
+++ test/gem_test.c
@@ -6,7 +6,7 @@ CTEST(gem_suite, search_of_zero_test)
 {
 	//Given
 	const int n = 4;
-	int field[n][n] = {{12, 5, 8, 7}, {11, 2, 4, 14}, {13, 6, 1, 0}, {10, 9, 15, 3}};
+	int field[4][4] = {{12, 5, 8, 7}, {11, 2, 4, 14}, {13, 6, 1, 0}, {10, 9, 15, 3}};
 
 	//When
 	int a = 0,  b = 0;
